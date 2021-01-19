9e13083eb1443f4ad1d7f5d66479df6802e60549
Yassine ROUISSI <rouissi@agridata-consulting.com>
2021-01-19 16:37:40 +0100

    save

--- src/app/services/ouvriers/ouvriers.service.ts
+++ src/app/services/ouvriers/ouvriers.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OuvriersService {
-  url = "//agridata.hopto.org:9010/agridata-lga-backend/api/"
+  url = "http://agridata.hopto.org:9010/agridata-lga-backend/api/"
   
   constructor(private http:HttpClient){
     console.log(this.url)
